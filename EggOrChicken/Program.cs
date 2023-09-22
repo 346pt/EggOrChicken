093d28f35d3e4ace1765597b326ab6c030c606e7
Антисептик <@DESKTOP-KTN6EI8>
2023-09-22 12:37:32 +0300

    Добавьте файлы проекта.

---
+++ EggOrChicken/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace EggOrChicken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread chicken = new(ThrChicken);
+            chicken.Start();
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine("Яйцо");
+            }
+            ThrAns(chicken);
+        }
+
+            static void ThrChicken()
+            {
+                for (int i = 0; i < 200; i++)
+                {
+                    Console.WriteLine("Курица");
+                }
+            }
+            static void ThrAns(Thread chicken)
+            {
+                Thread.Sleep(10);
+                bool ans = chicken.IsAlive;
+                Console.WriteLine(ans ? "Спор Окончен. Победила Курица." : "Спор окончен. Победило Яйцо.");
+            }
+        
+    }
+}
